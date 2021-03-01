50db58ab5f7f8ea0e6a4a859120a127a905cc9f8
JMIDEV <53763238+JMIDEV@users.noreply.github.com>
2021-03-01 16:38:21 +0100

    tyrtytrytrytrytr

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function openapp(appurl){
 
-    var myWindow = window.open(appurl, "", "width=1280,height=720");
+    var myWindow = window.open("", "", "width=1280, height=720, menubar=no, status=no, titlebar=no, status=no, toolbar=no, location=no, adressbar=no").location.href = appurl;
 
 }
