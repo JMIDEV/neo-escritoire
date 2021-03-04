643ff7877622ed584c25b42ae0a6e2f3cc803413
JMIDEV <53763238+JMIDEV@users.noreply.github.com>
2021-03-04 16:41:26 +0100

    ttttttttttttttttty

---
+++ settings.js
@@ -0,0 +1,40 @@
+var BGselected = 0;
+
+function checkBG(){
+
+    $('#mainbody').css('background', "url('Backgrounds/" + BGselected + ".jpg')");
+    
+    /*if(BGselected == 0){
+
+        $('#mainbody').css('background', "url('Backgrounds/0.jpg')");
+
+    }
+
+    else if(BGselected == 1){
+
+        $('#mainbody').css('background', "url('Backgrounds/1.jpg')");
+
+    }
+
+    else if(BGselected == 2){
+
+        $('#mainbody').css('background', "url('Backgrounds/2.jpg')");
+
+    }*/
+
+    $('#mainbody').css('background-repeat', "no-repeat");
+    $('#mainbody').css('background-position', "center");
+    $('#mainbody').css('background-size', "cover");
+
+    console.log("hey");
+
+}
+
+checkBG();
+
+function changeBG(bgnum){
+    
+    window.parent.BGselected = bgnum;
+    window.parent.checkBG();
+
+}
